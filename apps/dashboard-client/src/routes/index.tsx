53e48d279eac9169805fa00c5815ed47438381a7
Fazlul Hoque <fazlul@myalice.ai>
2025-10-24 11:16:55 +0600

    nice

--- apps/dashboard-client/src/routes/index.tsx
+++
@@ -1,6 +0,0 @@
-import LandingPage from '@/pages/landingPage/NewLandingPage';
-import { createFileRoute } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/')({
-  component: LandingPage,
-});
